79714a03bb8793f44458769a85defe8060c224af
Rafli Baihaqi <54907743+RafliBaihaqi@users.noreply.github.com>
2023-12-20 10:40:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ app.post('/FeedbackEmail', async (req, res) => {
 
     let mailOptions = {
       from: userEmail, 
-      to: 'ch2ps145@gmail.com', 
+      to: 'baihaqirafli30@gmail.com', 
       subject: 'New Message from User',
       text: `Message from ${userEmail}: ${userMessage}`
     };
