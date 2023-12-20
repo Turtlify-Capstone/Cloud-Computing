82ebc13e6e6255d1d33e2707f5c894bfee9c0590
nrd141 <nrd141@gmail.com>
2023-12-20 20:05:14 +0700

    update model

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import mysql.connector
 from mysql.connector import Error
 
 # Load TensorFlow model from URL
-url = "https://storage.googleapis.com/turtlifystorage/model_4_kelas.h5"
+url = "https://storage.googleapis.com/turtlifystorage/model_6_class.h5"
 response = requests.get(url)
 model_content = response.content
 with h5py.File(io.BytesIO(model_content), "r") as f:
